df6e0d864e0e76a0f3099d671a8de3745485c61f
Vladislav Stoyanov <methodlp@gmail.com>
2025-09-06 06:28:53 +0300

    Refactor docker-entrypoint.sh for improved readability and consistency in optional argument handling

--- container_files/docker-entrypoint.sh
+++ container_files/docker-entrypoint.sh
@@ -18,38 +18,29 @@ $HOME/azahar-room \
 s_password="${AZAHAR_PASSWORD}"
 
 add_optional_arg() {
-  while [[ "$#" -gt 0 ]]; do
+  while [ "$#" -gt 0 ]; do
     s_command="$s_command $1"
     shift
   done
 }
 
-if [ ! "x$AZAHAR_ROOMDESC" = "x" ]; then
-  add_optional_arg "--room-description" "\"${AZAHAR_ROOMDESC}\""
+# Optional parameters
+[ ! "x$AZAHAR_ROOMDESC" = "x" ] && add_optional_arg "--room-description" "\"${AZAHAR_ROOMDESC}\""
+[ ! "x$AZAHAR_PREFGAMEID" = "x" ] && add_optional_arg "--preferred-app-id" "\"${AZAHAR_PREFGAMEID}\""
+
+# Username for announce
+[ ! "x$AZAHAR_USERNAME" = "x" ] && add_optional_arg "--username" "\"${AZAHAR_USERNAME}\""
+
+# Password handling
+if [ "x$s_password" = "x" ] && [ -f "/run/secrets/azaharroom" ]; then
+  s_password=$(cat "/run/secrets/azaharroom")
 fi
+[ ! "x$s_password" = "x" ] && add_optional_arg "--password" "\"${s_password}\""
 
-if [ ! "x$AZAHAR_PREFGAMEID" = "x" ]; then
-  add_optional_arg "--preferred-app-id" "\"${AZAHAR_PREFGAMEID}\""
-fi
-
-if [ "x$s_password" = "x" ] \
-  && [ -f "/run/secrets/azaharroom" ]; then
-  s_password=$(cat "/run/secrets/azaharoom")
-fi
-
-if [ ! "x$s_password" = "x" ]; then
-  add_optional_arg "--password" "\"${s_password}\""
-fi
-
-if [ ! "x$AZAHAR_ISPUBLIC" = "x" ] \
-  && [ $AZAHAR_ISPUBLIC = 1 ]; then
-  if [ ! "x$AZAHAR_TOKEN" = "x" ]; then
-    add_optional_arg "--token" "\"${AZAHAR_TOKEN}\""
-  fi
-
-  if [ ! "x$AZAHAR_WEBAPIURL" = "x" ]; then
-    add_optional_arg "--web-api-url" "\"${AZAHAR_WEBAPIURL}\""
-  fi
+# Public server with API/Token
+if [ ! "x$AZAHAR_ISPUBLIC" = "x" ] && [ $AZAHAR_ISPUBLIC = 1 ]; then
+  [ ! "x$AZAHAR_TOKEN" = "x" ] && add_optional_arg "--token" "\"${AZAHAR_TOKEN}\""
+  [ ! "x$AZAHAR_WEBAPIURL" = "x" ] && add_optional_arg "--web-api-url" "\"${AZAHAR_WEBAPIURL}\""
 fi
 
 print_header() {
@@ -60,6 +51,7 @@ print_header() {
   [ $AZAHAR_ISPUBLIC = 1 ] && room_public="Yes" || room_public="No"
   [ ! "x$AZAHAR_PREFGAMEID" = "x" ] && room_pgid="${AZAHAR_PREFGAMEID}" || room_pgid="(unset)"
   [ ! "x$AZAHAR_WEBAPIURL" = "x" ] && room_api="${AZAHAR_WEBAPIURL}" || room_api="(unset)"
+  [ ! "x$AZAHAR_USERNAME" = "x" ] && room_user="${AZAHAR_USERNAME}" || room_user="(unset)"
 
   printf "Azahar Dedicated Server\n"
   printf "$pf" "Port:" "${AZAHAR_PORT}"
@@ -73,6 +65,7 @@ print_header() {
   printf "$pf" "Banlist File:" "${AZAHAR_BANLISTFILE}"
   printf "$pf" "Log File:" "${AZAHAR_LOGFILE}"
   printf "$pf" "Web API URL:" "${room_api}"
+  printf "$pf" "Username:" "${room_user}"
   printf "\n"
 }
 
